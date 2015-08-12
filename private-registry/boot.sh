6231b2ec1da3ac94201f0ffb0dd93c7d2d49f798
Ramiro Calle <ramiro.calle@rpharms.com>
2015-08-12 16:46:23 +0100

    API-135 : Add etcd client

--- private-registry/boot.sh
+++ private-registry/boot.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-mkdir -p /certs/
+function getFileFromEtcd {
+	filePath=$1
+	if [ ! -e "$filePath" ]
+	then
+	  etcdctl get "${filePath//\/[.]/\/_}" > /dev/null
+	  if [ "$?" = "0" ]
+	  then
+	    mkdir -p $(dirname "${filePath}")
+	    etcdctl get "${filePath//\/[.]/\/_}" > "$filePath"
+	    echo "[boot] get $filePath from etcd"
+	  fi
+	fi
+}
 
-PUBLIC="/certs/public.crt"
-PRIVATE="/certs/private.key"
+echo "[$0] Get files from etcd"
 
-ETCD_BASE_KEY=$1
+for file in "$@"
+do
+    getFileFromEtcd "$file"
+done
 
-echo "[boot] check SSL file"
-
-if [ ! -e "$PUBLIC" ] || [ ! -e "$PRIVATE" ]
-then
-  etcdctl get "$ETCD_BASE_KEY/public.crt" > /dev/null
-  publicExists="$?"
-  etcdctl get "$ETCD_BASE_KEY/private.key" > /dev/null
-  privateExists="$?"
-
-  if [ "$publicExists" = "0" ] && [ "$privateExists" = "0" ]
-  then
-    etcdctl get "$ETCD_BASE_KEY/public.crt" > "$PUBLIC"
-    etcdctl get "$ETCD_BASE_KEY/private.key" > "$PRIVATE"
-    echo "[boot] get $PUBLIC from etcd"
-    echo "[boot] get $PRIVATE from etcd"
-    export REGISTRY_HTTP_TLS_CERTIFICATE="$PUBLIC"
-    export REGISTRY_HTTP_TLS_KEY="$PRIVATE"
-  fi
-fi
-
-echo "[boot] finish $0."
+echo "[$0] finish."

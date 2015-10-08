4455677f358ebce26af9b81e1288681ae3b91636
Ramiro Calle <ramiro.calle@rpharms.com>
2015-10-08 15:28:36 +0100

    get config files from etcd

--- openresty/boot.sh
+++ openresty/boot.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+baseKey=$ETCD_BOOTSTRAP_BASE_KEY
+
+etcdctl get "${baseKey}" > /dev/null
+if [ "$?" = "0" ]
+then
+	echo "[boot] bootstrap key $baseKey"
+  for key in  $(etcdctl ls -p --recursive ${baseKey}  | grep -v "/$" )
+  do
+      filePath=${key#$baseKey}
+      if [ ! -e "$filePath" ]
+      then
+          echo "[boot] try to get ${filePath}"
+          if [ ! -d $(dirname "${filePath}") ]
+          then
+              mkdir -p $(dirname "${filePath}")
+          fi
+          etcdctl get "${key}" > "$filePath"
+          echo "[boot] get $filePath from etcd"
+      fi
+  done
+fi
+
 # Fail hard and fast
 set -eo pipefail
 

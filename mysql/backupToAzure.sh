7c926580a429808843d4142d1f0d8419c001ce81
rrramiro <ramiro.calle@rpharms.com>
2016-09-06 16:22:04 +0100

    Fix backup script

--- mysql/backupToAzure.sh
+++ mysql/backupToAzure.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+ #!/bin/bash
 
 FILE="${MYSQL_DATABASE}-$(date +"%Y%m%d%H%M%S").sql"
 COMPRESSED_FILE="${FILE}.gz"
 rm *.gz || true
 
-mysqldump --opt --user=root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} | gzip -9 > "${COMPRESSED_FILE}"
+mysqldump --opt --protocol=TCP --host=127.0.0.1 --user=root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} | gzip -9 > "${COMPRESSED_FILE}"
 ls -l ${COMPRESSED_FILE}
 
 azurectl --account-name "${AZURE_ACCOUNT_NAME}" --account-key "${AZURE_ACCOUNT_KEY}" upload --container "${AZURE_CONTAINER}" --blob "backup/${COMPRESSED_FILE}" "${COMPRESSED_FILE}"

78074eb29538e49aad6809eaab4d1b82ff01285a
rrramiro <ramiro.calle@rpharms.com>
2016-10-06 16:42:31 +0100

    Update backup and restore scripts for all databases on the instance

--- mysql/backupToAzure.sh
+++ mysql/backupToAzure.sh
@@ -1,9 +1,15 @@
  #!/bin/bash
 
-FILE="${MYSQL_DATABASE}-$(date +"%Y%m%d%H%M%S").sql"
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+FILE="${1}-$(date +"%Y%m%d%H%M%S").sql"
 COMPRESSED_FILE="${FILE}.gz"
 
-mysqldump --opt --protocol=TCP --host=${MYSQL_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} | gzip -9 > "${COMPRESSED_FILE}"
+mysqldump --opt --protocol=TCP --host=${MYSQL_HOST} --user=root --password=${MYSQL_ROOT_PASSWORD} --all-databases | gzip -9 > "${COMPRESSED_FILE}"
 ls -l ${COMPRESSED_FILE}
 
 azurectl --account-name "${AZURE_ACCOUNT_NAME}" --account-key "${AZURE_ACCOUNT_KEY}" upload --container "${AZURE_CONTAINER}" --blob "backup/${COMPRESSED_FILE}" "${COMPRESSED_FILE}"
--- mysql/restoreFromAzure.sh
+++ mysql/restoreFromAzure.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-VERSION=${1:-${MYSQL_RESTORE_VERSION}}
+RESTORE_FILE=${1:-${MYSQL_RESTORE_VERSION}}
 
-COMPRESSED_FILE="${MYSQL_DATABASE}-${VERSION}.sql.gz"
+COMPRESSED_FILE="${RESTORE_FILE}.sql.gz"
 FILE="${COMPRESSED_FILE%.gz}"
 rm *.sql || true
 
 azurectl --account-name "${AZURE_ACCOUNT_NAME}" --account-key "${AZURE_ACCOUNT_KEY}" download --container "${AZURE_CONTAINER}" --blob "backup/${COMPRESSED_FILE}" "${COMPRESSED_FILE}"
 gunzip "${COMPRESSED_FILE}"
 
-# ${MYSQL_ROOT_PASSWORD}
 
-mysql -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p${MYSQL_PASSWORD} "${MYSQL_DATABASE}" < "${FILE}"
+cat "${FILE}" <(echo "FLUSH PRIVILEGES ;") | mysql -h "${MYSQL_HOST}" -uroot -p${MYSQL_ROOT_PASSWORD} mysql
